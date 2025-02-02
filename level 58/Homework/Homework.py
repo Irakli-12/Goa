86badf2617c3b4d3dc3add4e4d841d3f6bb00c98
Irakli-12 <gegiairakli13@gmail.com>
2025-02-02 12:55:11 +0400

    commit

---
+++ level 58/Homework/Homework.py
@@ -0,0 +1,26 @@
+# 1)https://www.codewars.com/kata/58bf9bd943fadb2a980000a7
+# ვერ გავაკეთე
+# 2)https://www.codewars.com/kata/59342039eb450e39970000a6
+def odd_count(n):
+    return n // 2
+# 3)https://www.codewars.com/kata/56cd44e1aa4ac7879200010b
+def is_uppercase(inp):
+    for x in inp:
+        if x.islower():
+            return False
+    return True
+# 4)https://www.codewars.com/kata/53d16bd82578b1fb5b00128c
+def grader(score):
+    if score > 1 or score < 0.6:
+        return "F"
+    elif score >= 0.9:
+        return "A"
+    elif score >= 0.8:
+        return "B"
+    elif score >= 0.7:
+        return "C"
+    elif score >= 0.6:
+        return "D"
+# 5)https://www.codewars.com/kata/570669d8cb7293a2d1001473
+def if_chuck_says_so():
+    return not True
