79904343c6e17a51b6af7b3a7b98d3cd7284bd73
Irakli-12 <gegiairakli13@gmail.com>
2025-02-24 17:06:57 +0400

    first commit

---
+++ Level 01/Homework/Homework.py
@@ -0,0 +1,24 @@
+name = "irakli"
+surname = "gegia"
+age = 11
+country = "georgia,"
+print (name + " " + surname + " " + "is" + " " + str(age) + " " + "years old ." + " " +  "he lives in martvili"  + " " + country)
+
+list1=["ikako", 12, ]
+print(list1[1])
+name = "Lionel"
+surname = "Messi"
+age = 36
+print ("My favourite footballer is" + " " + name + " " + surname + "." + " " "he is" + " " + str(age) + " " + "years old." + " " + "he plays in inter miami.")
+word = 4
+word1 = 10.4
+print(word + word1)
+name = "irakli"
+surname = "gegia"
+print(name + " " + surname)
+word2 = "2"
+word3 = 5
+print(str(word2) + word3)
+print("String არის ბრჭყალებში მოცემული სიმბოლო რომელსაც ვანიჭებთ მნიშვნელობას.")
+print("ინტეჯერი არის მთელი რიცხვი, მაგალითად 1, 2, 3, 4, 5 და ასეშემდეგ")
+print("Float არის ათწილადი რიცხვი მაგალითად 10.3 , 10.9")
