d05a4512d0a22678344e82850324f9a3ee7070a2
Irakli-12 <gegiairakli13@gmail.com>
2025-03-03 17:47:26 +0400

    second commit

---
+++ Level 03/Classwork/Classwork.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter Your Number: "))
+num2 = int(input("Enter Your Second Number: "))
+if num1 > num2:
+    print("First Number Is More Than Second One")
+else:
+    print("Second Number Is Higher")
