865e0b4b9b9e4312a0e59511d25aa3bc170bf803
Irakli-12 <gegiairakli13@gmail.com>
2025-04-07 21:16:49 +0400

    second commit

---
+++ Level 18/Homework/Homework.py
@@ -0,0 +1,66 @@
+# 1) ჩამოწერეთ ყველა პითონის ჩაშენებული ფუნქცია, რაც დღს განვიხილეთ.
+# print()
+# input()
+# type()
+# 2) დაწერეთ ამ ფუნქციების ორ-ორი მაგალითი და ახსენით თუ როგორ მუშაობს თითოეული.
+# print გამოძახებას ნიშნავს ანუ ჩვენ გამოვიძახეთ hello.
+print("hello")
+print("Bye")
+# input მომხმარებელს უფლებას აძლევს რომ რაიმე ტერმინალში შეიტანოს
+input("Enter Number: ")
+input("Enter Name: ")
+# type data type-ს მნიშვნელობას გვაძლევს
+print(type("hello"))
+print(type(5))
+
+# 3) ეცადეთ, რომ გააკეთოთ ამ ფუნქციების Copy ფუნქციები თქვენი ხელით.
+
+# 4) შექმენით სია სადაც შეინახავთ 5 რიცხვს, მომხმარებელს აარჩევინეთ ამ 
+# სიიდან ერთ-ერთი რიცხვი და დათვალეთ თუ რამდენჯერ მეორდება ეს რიცხვი სიაში.
+
+# 5) შექმენით სია, სადაც დაამატებთ მინიმუმ 5 ელემენტს. მომხმარებელს ჰკითხე სურს თუ არა სიის გასუფთავება.
+#  თუ დაგთანხმდება გაასუფთავე სია თუ არა მაშინ ჩვეულებრივად დაბეჭდე ეს სია ტერმინალში.
+
+# 6) შექმენით სია სახელად Fruits და ჩაამატეთ მასში ხილი. მომხმარებელს შემოატანინეთ ინდექსი 
+# და შემდეგ  ამ ინდექსზე მდგომი ელემენტი ამოშალე სიიდან. საბოლოოდ დაბეჭდე სიის საბოლოო ვერსია.
+
+# გააკეთეთ Codewars-ები: 
+
+# 7) https://www.codewars.com/kata/5a023c426975981341000014/train/python (ამ ამოცანის გასაკეთებლად მათემატიკური ცოდნა დაგჭირდებათ,
+# ამიტომ მოიძიეთ ინფორმაცია 
+# თუ როგორ ვიპოვოთ სამკუთხედის მესამე კუთხის გრადუსი, იმ შემთხვევაში თუ დანარჩენი ორი კუთხის გრადუსი ჩვენთვის უკვე ცნობილია).
+
+# 8) https://www.codewars.com/kata/5951d30ce99cf2467e000013/train/python (მესამე ამოცანის მსგავსად, 
+# ამ kata-ს ამოსახსნელად დაგჭირდებათ მათემატიკური ცოდნა. კერძოდ პითაგორას თეორემა. 
+# მოიძიეთ ინფორმაცია პითაგორას თეორემის შესახებ და ამის მიხედვით ამოხსენით ეს codewars-ი).
+
+# 9) https://www.codewars.com/kata/5715eaedb436cf5606000381/train/python
+# def positive_sum(arr):
+#     num = 0
+#     for i in arr:
+#         if i > 0:
+#             num = num + i
+#     return num
+
+# 10) https://www.codewars.com/kata/5ad0d8356165e63c140014d4
+# def final_grade(exam, projects):
+#     if exam > 90 or projects > 10:
+#         return 100
+#     elif exam > 75 and projects >= 5:
+#         return 90
+#     elif exam > 50 and projects >= 2:
+#         return 75
+#     else:
+#         return 0
+# 11) https://www.codewars.com/kata/5834fec22fb0ba7d080000e8/train/python
+
+# 12)https://www.codewars.com/kata/566dc566f6ea9a14b500007b/train/python
+# def kata_13_december(lst): 
+#     odd_numbers = []
+#     for num in lst:
+#         if num % 2 != 0:
+#             odd_numbers.append(num)
+    # return odd_numbers
+# 13) https://www.codewars.com/kata/559590633066759614000063/train/python
+# def min_max(lst):
+    # return [min(lst), max(lst)]
