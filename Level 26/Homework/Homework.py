bffe4fbca29f7bc8d446f7fc348847e7a01196d0
Irakli-12 <gegiairakli13@gmail.com>
2025-04-27 23:01:44 +0400

    second commit

---
+++ Level 26/Homework/Homework.py
@@ -0,0 +1,10 @@
+# ახსენით თუ რა არის (Server,RAM,Internet,HTTP,HTTPS,DNS,Hosting) და როგორ 
+# მუშაობს თითოეული მათგანი, ასევე დახატეთ paint-ის ან Kleki-ის საშუალებით და კარგად გაიაზრეთ
+# Server უკავშირდება სხვა Device-ებს
+# Ram არის ოპერატიული მეხსიერება
+# Internet არის გლობალური ქსელი
+# HTTP-თი გადაეცემა ვებსაიტებს ინფორმაციას
+# გადახედეთ შემდეგ რესურსებს
+# Done
+# 1) https://www.geeksforgeeks.org/what-is-http/
+# 2) https://www.geeksforgeeks.org/what-is-a-web-server-working-and-architecture/
