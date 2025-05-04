b8d101d42c95de44a4f16d42c0aecae990e4a466
Irakli-12 <gegiairakli13@gmail.com>
2025-05-04 21:51:47 +0400

    commit

---
+++ Level 28/Classwork/Classwork.py
@@ -0,0 +1,15 @@
+# 7 kyu
+# https://www.codewars.com/kata/5356ad2cbb858025d800111d/train/python
+# def cap_me(arr):
+#     names1 = []
+#     for name in arr:
+#         names1.append(name.capitalize())
+    # return names1
+# https://www.codewars.com/kata/54b81566cd7f51408300022d/train/python
+
+# 6 kyu
+# https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/train/python
+# https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/python
+# def create_phone_number(n):
+    # return f"({n[0]}{n[1]}{n[2]}) {n[3]}{n[4]}{n[5]}-{n[6]}{n[7]}{n[8]}{n[9]}"
+# https://www.codewars.com/kata/517abf86da9663f1d2000003/train/python
---
+++ Level 28/Homework/Homework.py
@@ -0,0 +1,25 @@
+# 8 kyu
+# https://www.codewars.com/kata/5966e33c4e686b508700002d/train/python
+# def sum_str(a, b):
+#     if a == "":
+#         a = "0"
+#     if b == "":
+#         b = "0"
+    
+#     res = int(a) + int(b)
+#     return str(res)
+# https://www.codewars.com/kata/572b6b2772a38bc1e700007a/train/python (ord - ფუნქციაზე გადახედეთ შემდეგ რესურს https://www.w3schools.com/python/ref_func_ord.asp)
+# def uni_total(s):
+#     t = 0
+#     for r in s:
+#         t += ord(r)
+#     return t
+
+# 7 kyu
+# https://www.codewars.com/kata/52f3149496de55aded000410/train/python
+# https://www.codewars.com/kata/56a921fa8c5167d8e7000053/train/python
+# https://www.codewars.com/kata/56582133c932d8239900002e/train/python
+
+# 6 kyu
+# https://www.codewars.com/kata/5264d2b162488dc400000001/train/python
+# https://www.codewars.com/kata/53697be005f803751e0015aa/train/python
